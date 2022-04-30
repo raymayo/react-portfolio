03002f60a97e42d7c8795e5ebe6c04ee149d29ea
Ray Mayo <ormillada.jr@gmail.com>
2022-05-01 01:10:21 +0800

    fix logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import navCss from '../Stylesheets/navBar.module.css'
+import Logo from '../Logo.svg';
 
 const Navbar = () => {
     return (
         <div id={navCss.headerContainer}>
             <Link id={navCss.logoLink} to="/">
-                <img id={navCss.Logo} src="Logo.svg" alt="Logo" /></Link>
+                <img id={navCss.Logo} src={Logo} alt="" /></Link>
             <a href="RayMayo-Resume.png" id={navCss.resume} className={navCss.download} download>RESUME</a>
         </div>
     )
